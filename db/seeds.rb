3541a91385f94f0452b2aa05a5d629836b08b015
Sandra Vilder <sandra.vilder@gmail.com>
2020-01-23 11:06:33 +0100

    added logos and icons / seed making / layout in construction : right bar

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,39 @@
-user = User.create(email: "yo@yo.com", password: "yoyoyo", username: "BigYo")
+puts "start seeding"
 
-ToDoList.create(user_id: 1)
-# title
 
-Task.create(to_do_list_id: 1, description: "faire à manger")
-# status
+if Rails.env.development?
 
-ShortNote.create(user_id: 1, description: "Shoubidoubidou")
+  puts "cleaning database"
 
-Objectif.create(user_id: 1, title: "atteindre la lune", date: "demain")
-# description
+  User.destroy_all
+  ToDoList.destroy_all
+  Task.destroy_all
+  ShortNote.destroy_all
+  Objectif.destroy_all
+  Mission.destroy_all
+  LongNote.destroy_all
 
-Mission.create(user_id: 1, title: "Maxime Hervé", client_name: "Maxime Hervé")
-# description, time_spent, money_earned (integer), note
+  puts "creating fresh new instances"
 
-LongNote.create(user_id: 1, title: "Siteweb RogerVilder - compte rendu d'expérience", )
-# description
+  user = User.create!(email: "yo@yo.com", password: "yoyoyo", username: "BigYo")
+
+  ToDoList.create!(user_id: 1, title: "Job stuffs")
+
+
+  Task.create!(to_do_list_id: 1, description: "faire à manger")
+  # status
+
+  ShortNote.create!(user_id: 1, description: "Shoubidoubidou")
+
+  Objectif.create!(user_id: 1, title: "atteindre la lune", date: "demain")
+  # description
+
+  Mission.create!(user_id: 1, title: "Maxime Hervé", client_name: "Maxime Hervé")
+  # description, time_spent, money_earned (integer), note
+
+  LongNote.create!(user_id: 1, title: "Siteweb RogerVilder - compte rendu d'expérience", )
+  # description
+
+end
+
+puts "end of seed"
