2d8976ee4d3c7e3e0865c7a719bbe0a04b133108
Sandra Vilder <sandra.vilder@gmail.com>
2020-01-24 12:27:56 +0100

    tools methods /tools design

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def calc_percentage(number)
+    return number * 0,24
+  end
+
+  def multiply_by_eigth(number)
+    return number * 8
+  end
+
+  def divide_by_eight(number)
+    return number / 8
+  end
 end
