ad79ca03fb685b5496f868f18857630614c366b7
Sandra Vilder <sandra.vilder@gmail.com>
2020-02-17 21:28:24 +0100

    began rule of eight tool but js is darker when it's late

---
+++ app/javascript/components/rule_of_eight.js
@@ -0,0 +1,11 @@
+const ruleOfEight = () => {
+  const input = document.querySelector('#eight');
+  const button = document.querySelector('#go-eight');
+  const result = document.querySelector('#result-eight');
+  console.log(input);
+  console.log(button);
+  console.log(result;
+}
+
+
+export { ruleOfEight };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,3 +2,6 @@ import "bootstrap";
 
 import { calculPercent } from "../components/calcul_percent";
 calculPercent();
+
+import { ruleOfEight } from '../components/rule-of-eight';
+ruleOfEight();
