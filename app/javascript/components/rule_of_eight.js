5f20f93a07979dccd4c31017f0200162055f8a98
Sandra Vilder <sandra.vilder@gmail.com>
2020-02-18 12:19:24 +0100

    rule of eight working

--- app/javascript/components/rule_of_eight.js
+++ app/javascript/components/rule_of_eight.js
@@ -2,42 +2,41 @@ const ruleOfEight = () => {
   const input = document.querySelector('#input-eight');
   const button = document.querySelector('#go-eight');
   const result = document.querySelector('#result-eight');
+  const result_sign = document.querySelector('#result-sign');
 
   const findUpperValue = (number) => {
-    console.log(number);
-      let sum = 0;
-      do {
-        number++;
-        let sum = number % 8
-      } while (sum !== 0);
-      console.log(number);
+    let sum = 0;
+    do {
+      number++;
+      sum = number % 8
+    } while (sum !== 0);
+    result.insertAdjacentHTML('beforeend', `${number}`);
   }
 
+  const findLowerValue = (number) => {
+    let sum = 0;
+    do {
+      number--;
+      sum = number % 8
+    } while (sum !== 0);
+    result.insertAdjacentHTML('beforeend', `${number} or `);
+  }
 
-    //   let test = true
-    // do {
-    //   number++;
-    //   let divided = number / 8;
-    //   let test = Number.isInteger(divided);
-    //   result = number % 8;
-    //   console.log(result)
-    // } while (test === false);
-    // console.log(number);
 
   const multipleOfEight = () => {
     let number = input.value;
     let divided = number / 8;
     let test = Number.isInteger(divided);
+    result.innerHTML = "";
     if (test === false) {
       console.log("faux !");
-      result.innerHTML = "";
-      result.insertAdjacentHTML('beforeend', `<i class="far fa-times-circle"></i>`);
+      result_sign.insertAdjacentHTML('beforeend', `<i class="far fa-times-circle"></i>`);
     } else {
       console.log("vrai !");
-      result.innerHTML = "";
-      result.insertAdjacentHTML('beforeend', `<i class="far fa-check-circle"></i>`);
+      result_sign.insertAdjacentHTML('beforeend', `<i class="far fa-check-circle"></i>`);
     }
     // suggestion
+    findLowerValue(number);
     findUpperValue(number);
   }
 
