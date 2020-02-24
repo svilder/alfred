8031c2ecf681fcf4c58da9314483fb60cda57d1c
Sandra Vilder <sandra.vilder@gmail.com>
2020-02-24 18:09:35 +0100

    rebuild ajax in controller as it was

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,6 +5,11 @@ class TasksController < ApplicationController
   def new
     @to_do_list = ToDoList.find(params[:to_do_list_id])
     @task = Task.new
+    authorize @task
+    respond_to do |format|
+      format.html { redirect_to new_to_do_list_task_path(@to_do_list) }
+      format.js
+    end
   end
 
   def create
@@ -13,20 +18,36 @@ class TasksController < ApplicationController
     authorize @task
 
     if @task.save
-      redirect_to to_do_list_path(params[:to_do_list_id])
+      respond_to do |format|
+        format.html { redirect_to to_do_list_path(params[:to_do_list_id]) }
+        format.js
+      end
     else
-      render 'to_do_lists/show'
+      respond_to do |format|
+        format.html { render 'to_do_lists/show' }
+        format.js
+      end
     end
   end
 
   def edit
+    respond_to do |format|
+      format.html { redirect_to edit_task_path(@task) }
+      format.js
+    end
   end
 
   def update
     if @task.update(task_params)
-      redirect_to to_do_list_path(@task.to_do_list_id)
+      respond_to do |format|
+        format.html { to_do_list_path(@task.to_do_list_id) }
+        format.js
+      end
     else
-      render 'to_do_lists/show'
+      respond_to do |format|
+        format.html { render 'to_do_lists/show' }
+        format.js
+      end
     end
   end
 
@@ -42,7 +63,10 @@ class TasksController < ApplicationController
 
   def destroy
     @task.destroy
-    redirect_to to_do_list_path(@task.to_do_list_id)
+    respond_to do |format|
+      format.html { redirect_to to_do_list_path(@task.to_do_list_id) }
+      format.js
+    end
   end
 
   private
