324e82cadcbf6d24e0d77be8241a9566b842ace4
Sandra Vilder <sandra.vilder@gmail.com>
2020-03-23 15:03:02 +0100

    toggle rightbar

--- app/javascript/components/toggle_tools.js
+++ app/javascript/components/toggle_tools.js
@@ -5,16 +5,21 @@ const toggleTools = () => {
   const mainWrapper = document.querySelector(".main-wrapper");
 
   const toggleSideBar = () => {
-    if (rightBar.style.display === "flex") {
-      rightBar.style.display = "none";
-      mainWrapper.style.marginRight = "72px";
-      button.style.right = "0";
-    } else {
-      rightBar.style.display = "flex";
-      mainWrapper.style.marginRight = "320px";
-      button.style.right = "248px";
 
-    }
+    console.log(rightBar);
+    rightBar.classList.toggle("hide");
+    mainWrapper.classList.toggle("margin-right");
+    button.classList.toggle("right-zero");
+
+    // if (rightBar.style.display === "flex") {
+    //   rightBar.style.display = "none";
+    //   mainWrapper.style.marginRight = "72px";
+    //   button.style.right = "0";
+    // } else {
+    //   rightBar.style.display = "flex";
+    //   mainWrapper.style.marginRight = "320px";
+    //   button.style.right = "248px";
+    // }
   }
 
   button.addEventListener('click', toggleSideBar);
