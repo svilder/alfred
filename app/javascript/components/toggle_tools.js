65e4c1ab45600edf084d4df2901319d4fb48d894
Sandra Vilder <sandra.vilder@gmail.com>
2020-03-24 11:03:01 +0100

    handle turbolinks:load

--- app/javascript/components/toggle_tools.js
+++ app/javascript/components/toggle_tools.js
@@ -10,16 +10,6 @@ const toggleTools = () => {
     rightBar.classList.toggle("hide");
     mainWrapper.classList.toggle("margin-right");
     button.classList.toggle("right-zero");
-
-    // if (rightBar.style.display === "flex") {
-    //   rightBar.style.display = "none";
-    //   mainWrapper.style.marginRight = "72px";
-    //   button.style.right = "0";
-    // } else {
-    //   rightBar.style.display = "flex";
-    //   mainWrapper.style.marginRight = "320px";
-    //   button.style.right = "248px";
-    // }
   }
 
   button.addEventListener('click', toggleSideBar);
