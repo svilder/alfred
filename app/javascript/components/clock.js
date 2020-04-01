25720947b1efce6b2fe9f264d68c018841dfe1f7
Sandra Vilder <sandra.vilder@gmail.com>
2020-04-01 15:55:42 +0200

    fix clock

--- app/javascript/components/clock.js
+++ app/javascript/components/clock.js
@@ -6,14 +6,21 @@ const clock = () => {
 
   const displayResult = (hours, mins, seconds) => {
     console.log("displaying result");
+    buttonStart.classList.add("mr-3");
     chronoResult.innerHTML = "";
     chronoResult.insertAdjacentHTML('beforeend', `<p>${hours} : ${mins} : ${seconds}</p>`);
   }
 
   const updateButton = () => {
     console.log("updating button");
+    chronoResult.innerHTML = "";
     buttonStart.classList.add("hide");
     buttonStop.classList.remove("hide");
+  }
+
+  const resetButton = () => {
+    buttonStart.classList.remove("hide");
+    buttonStop.classList.add("hide");
   }
 
   const startClock = (event) => {
@@ -27,16 +34,21 @@ const clock = () => {
       console.log(endTime);
 
       const calcul = endTime - beginTime;
-      const seconds = Math.floor(calcul / 1000);
-      const mins = Math.floor(seconds / 60);
-      const hours = Math.floor(mins / 60);
+      let seconds = Math.floor(calcul / 1000);
+      let mins = Math.floor(seconds / 60);
+      let hours = Math.floor(mins / 60);
+      if (seconds > 60) {
+        seconds = seconds - (mins * 60)
+      }
+      if (mins > 60) {
+        mins = mins - (hours * 60)
+      }
 
-      buttonStart.classList.remove("hide");
-      buttonStart.classList.add("mr-3");
-      buttonStop.classList.add("hide");
+      resetButton();
+      buttonStart.classList.remove("mr-3");
+
       displayResult(hours, mins, seconds);
     });
-
   }
 
   buttonStart.addEventListener('click', startClock);
