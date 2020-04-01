ebbc516b2fc06f70ad56bd68c1f9160cc0ecfbbb
Sandra Vilder <sandra.vilder@gmail.com>
2020-04-01 16:14:33 +0200

    add a clock

---
+++ app/javascript/components/chrono.js
@@ -0,0 +1,60 @@
+const chrono = () => {
+  // const chronoImage = document.querySelector(".chrono-image");
+  const buttonStart = document.querySelector('#start');
+  const buttonStop = document.querySelector("#stop");
+  buttonStop.classList.add("hide");
+  const chronoResult = document.querySelector("#chrono-result");
+
+  const displayResult = (hours, mins, seconds) => {
+    console.log("displaying result");
+    buttonStart.classList.add("mr-3");
+    chronoResult.innerHTML = "";
+    chronoResult.insertAdjacentHTML('beforeend', `<p>${hours} : ${mins} : ${seconds}</p>`);
+  }
+
+  const updateButton = () => {
+    console.log("updating button");
+    chronoResult.innerHTML = "";
+    buttonStart.classList.add("hide");
+    buttonStop.classList.remove("hide");
+    chronoImage.classList.add("hide");
+  }
+
+  const resetButton = () => {
+    buttonStart.classList.remove("hide");
+    buttonStop.classList.add("hide");
+    chronoImage.classList.remove("hide");
+  }
+
+  const startClock = (event) => {
+    console.log("inside the machiiiine");
+    const beginTime = new Date().getTime();
+    console.log(beginTime);
+    updateButton();
+    buttonStop.addEventListener('click', () => {
+      console.log("clicked on stop !")
+      const endTime = new Date().getTime();
+      console.log(endTime);
+
+      const calcul = endTime - beginTime;
+      let seconds = Math.floor(calcul / 1000);
+      let mins = Math.floor(seconds / 60);
+      let hours = Math.floor(mins / 60);
+      if (seconds > 60) {
+        seconds = seconds - (mins * 60)
+      }
+      if (mins > 60) {
+        mins = mins - (hours * 60)
+      }
+
+      resetButton();
+      buttonStart.classList.remove("mr-3");
+
+      displayResult(hours, mins, seconds);
+    });
+  }
+
+  buttonStart.addEventListener('click', startClock);
+}
+
+export { chrono };
--- app/javascript/components/clock.js
+++ app/javascript/components/clock.js
@@ -1,57 +1,28 @@
 const clock = () => {
-  const buttonStart = document.querySelector('#start');
-  const buttonStop = document.querySelector("#stop");
-  buttonStop.classList.add("hide");
-  const chronoResult = document.querySelector("#chrono-result");
+  const secondHand = document.querySelector('.second-hand');
+  const minsHand = document.querySelector('.min-hand');
+  const hourHand = document.querySelector('.hour-hand');
 
-  const displayResult = (hours, mins, seconds) => {
-    console.log("displaying result");
-    buttonStart.classList.add("mr-3");
-    chronoResult.innerHTML = "";
-    chronoResult.insertAdjacentHTML('beforeend', `<p>${hours} : ${mins} : ${seconds}</p>`);
+
+  const showTime = () => {
+    const now = new Date();
+
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+    const mins = now.getMinutes();
+    const minsDegrees = ((mins / 60) * 360) + ((seconds / 60) * 6) + 90;
+    minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+    const hour = now.getHours();
+    const hourDegrees = ((hour / 12) * 360) + ((mins / 60) * 30) + 90;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+    console.log(secondsDegrees, minsDegrees, hourDegrees);
   }
 
-  const updateButton = () => {
-    console.log("updating button");
-    chronoResult.innerHTML = "";
-    buttonStart.classList.add("hide");
-    buttonStop.classList.remove("hide");
-  }
-
-  const resetButton = () => {
-    buttonStart.classList.remove("hide");
-    buttonStop.classList.add("hide");
-  }
-
-  const startClock = (event) => {
-    console.log("inside the machiiiine");
-    const beginTime = new Date().getTime();
-    console.log(beginTime);
-    updateButton();
-    buttonStop.addEventListener('click', () => {
-      console.log("clicked on stop !")
-      const endTime = new Date().getTime();
-      console.log(endTime);
-
-      const calcul = endTime - beginTime;
-      let seconds = Math.floor(calcul / 1000);
-      let mins = Math.floor(seconds / 60);
-      let hours = Math.floor(mins / 60);
-      if (seconds > 60) {
-        seconds = seconds - (mins * 60)
-      }
-      if (mins > 60) {
-        mins = mins - (hours * 60)
-      }
-
-      resetButton();
-      buttonStart.classList.remove("mr-3");
-
-      displayResult(hours, mins, seconds);
-    });
-  }
-
-  buttonStart.addEventListener('click', startClock);
+  setInterval(showTime, 1000);
+  showTime();
 }
 
 export { clock };
