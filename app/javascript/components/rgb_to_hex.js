9a8611ba3dc6d1b2e2e5dd4a34fe90af6d6d2adf
Sandra Vilder <sandra.vilder@gmail.com>
2020-10-08 14:41:22 +0200

    get rid of tried values

--- app/javascript/components/rgb_to_hex.js
+++ app/javascript/components/rgb_to_hex.js
@@ -4,26 +4,7 @@ const rgbToHex = () => {
   const inputB = document.querySelector('#B');
   const button = document.querySelector('#go-hex');
   const result = document.querySelector('#result-hex');
-  const hex_valuesee = {
-    0:  '0',
-    1:  '1',
-    2:  '2',
-    3:  '3',
-    4:  '4',
-    5:  '5',
-    6:  '6',
-    7:  '7',
-    8:  '8',
-    9:  '9',
-    10: 'A',
-    11: 'B',
-    12: 'C',
-    13: 'D',
-    14: 'E',
-    15: 'F'
-  }
-
-  const hex_values = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
+  const hex_values = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
 
   const insertResult = (hex_value) => {
     result.innerHTML = "";
