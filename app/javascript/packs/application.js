7509656a77e358ecefb73f3a8efee4e456e52ec3
Sandra Vilder <sandra.vilder@gmail.com>
2020-10-14 17:43:00 +0200

    code button modified

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -30,7 +30,7 @@ import { toggleTools } from "../components/toggle_tools";
 import { dragToDos } from "../components/drag_to_dos";
 import { colorBackground } from "../components/color_background";
 import { rgbToHex } from "../components/rgb_to_hex";
-import { trixToolbarUnderlineButton } from "../components/trix_toolbar_underline_button";
+// import { trixToolbarUnderlineButton } from "../components/trix_toolbar_underline_button";
 
 document.addEventListener("turbolinks:load", function() {
   // calculPercent();
@@ -41,7 +41,7 @@ document.addEventListener("turbolinks:load", function() {
   dragToDos();
   colorBackground();
   rgbToHex();
-  trixToolbarUnderlineButton();
+  // trixToolbarUnderlineButton();
 })
 
 // Stimulus
@@ -49,3 +49,31 @@ document.addEventListener("turbolinks:load", function() {
 
 require("trix")
 require("@rails/actiontext")
+
+const Trix  = require("trix")
+console.log("Config", Trix.config);
+
+// Trix.config.blockAttributes.heading2 = {
+//   tagName: 'h2'
+// }
+Trix.config.textAttributes.code1 = {
+  tagName: 'pre'
+}
+Trix.config.textAttributes.underline = {
+  tagName: 'u'
+}
+
+addEventListener("trix-initialize", function(event) {
+  const toolbar = event.target.toolbarElement;
+  const buttonUnderlineHTML = '<button type="button" class="trix-button trix-button--my-icon" data-trix-attribute="underline" title="Underline" tabindex="-1"><i class="fas fa-underline"></i></button>';
+  const buttonCodeHTML = '<button type="button" class="trix-button trix-button--my-icon" data-trix-attribute="code1" title="Code" tabindex="-1"><i class="fas fa-terminal"></i></button>';
+
+  toolbar.querySelector(".trix-button--icon-italic").insertAdjacentHTML("afterend", buttonUnderlineHTML);
+  toolbar.querySelector(".trix-button--icon-code").remove();
+  // console.log("buttonHTML", toolbar.querySelector(".trix-button--icon-code"));
+  toolbar.querySelector(".trix-button--icon-quote").insertAdjacentHTML("afterend", buttonCodeHTML);
+});
+
+
+
+
